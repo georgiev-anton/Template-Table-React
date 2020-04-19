d5efb16bd8b6f87cd00fdc8f6e95c81b5ea77beb
Антон Георгиев <antongeorgiev@MacBook-Pro-Anton.local>
2020-04-20 00:29:59 +0500

    Updates

---
+++ precache-manifest.b0ff592238954324a05c201bcf79e5db.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c2c58d1d99f06063f420b014998ed2e",
+    "url": "/Template-Table-React/index.html"
+  },
+  {
+    "revision": "b1d7df04527f1260c9fa",
+    "url": "/Template-Table-React/static/css/main.a19e5013.chunk.css"
+  },
+  {
+    "revision": "81b013c06d0e9a203ce7",
+    "url": "/Template-Table-React/static/js/2.64c56871.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Template-Table-React/static/js/2.64c56871.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1d7df04527f1260c9fa",
+    "url": "/Template-Table-React/static/js/main.cd7404e9.chunk.js"
+  },
+  {
+    "revision": "abf61599835e7f3fcbc3",
+    "url": "/Template-Table-React/static/js/runtime-main.5dc76051.js"
+  }
+]);
